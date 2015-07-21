5e68fd4f189e50eca63bd6cc458b058f9a078994
Pardeep Kumar <pardeep.kumar@upr.edu>
2015-07-21 19:20:50 -0400

    pulling successful , plotting is left

--- pull_capture_data.py
+++ pull_capture_data.py
@@ -1,26 +1,19 @@
 import os
 import subprocess
-import fileinput
-import time
 
-# processing_foo1s = False
-#os.system("adb shell cat /sys/class/power_supply/battery/uevent")
+
 
 out1 = os.popen("pwd ").read()
 print out1
-var =70;
-f1= "SamCharg-4.1";
-
-
 output1 = os.popen("adb shell getprop | grep product.brand").read()
 output2 = os.popen("adb shell getprop | grep product.model").read()
-print output2
+
 
 
 output1 = output1.replace("[", "")
 output1 = output1.replace("]", "")
 output1=output1.split()
-print output1[1]
+#print output1[1]
 
 directory = "LOL"
 folder = "OLO"
@@ -41,6 +34,6 @@ elif "google" in str(output1[1]).lower():
 
 pull_command="adb pull "+str(directory)+"/Attribute-data/ '/home/larsip-ubuntu-2/Downloads/data-char/script-data/"+str(folder)+"' "		
 
-print pull_command
+#print pull_command
 os.system(pull_command)
 print "completed"
