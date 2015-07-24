32d3e65110be5ba39286469775d18ba931420ba9
Pardeep Kumar <pardeep.kumar@upr.edu>
2015-07-24 12:31:08 -0400

    changes

--- pull_capture_data.py
+++ pull_capture_data.py
@@ -2,21 +2,18 @@ import os
 import subprocess
 
 
-
 out1 = os.popen("pwd ").read()
 print out1
 output1 = os.popen("adb shell getprop | grep product.brand").read()
 output2 = os.popen("adb shell getprop | grep product.model").read()
 
 
-
+print output1
 output1 = output1.replace("[", "")
 output1 = output1.replace("]", "")
 output1=output1.split()
-#print output1[1]
+print output1[1]
 
-directory = "LOL"
-folder = "OLO"
 
 if "samsung" in str(output1[1]).lower():
 	print "Samsung S6"
@@ -30,7 +27,8 @@ elif "google" in str(output1[1]).lower():
 	print "nexus found"
 	folder = "Nexus"
 	directory= "/storage/sdcard0"
-
+else:
+	print "ADB is down"
 
 pull_command="adb pull "+str(directory)+"/Attribute-data/ '/home/larsip-ubuntu-2/Downloads/data-char/script-data/"+str(folder)+"' "		
 
